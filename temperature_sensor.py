743abc1392cf8ab711674c6ecd1b917917c8edb5
Alan Chen <alanc1995@gmail.com>
2017-08-24 00:50:01 -0400

    Variables and Fahrenheit

--- temperature_sensor.py
+++ temperature_sensor.py
@@ -12,13 +12,18 @@ import sys
 import Adafruit_DHT
 
 while True:
-    # Celsius Reading, Fahrenheit reading is located inside Adafruit_DHT library
+    # Celsius Reading
     # Try to grab a sensor reading.  Use the read_retry method which will retry up
     # to 15 times to get a sensor reading (waiting 2 seconds between each retry).
     # humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
-    humidity, temperature = Adafruit_DHT.read_retry(11, 4)
+    INPUT_PIN = 4
+    DHT_TYPE = Adafruit_DHT.DHT11
+
+    humidity, temperature = Adafruit_DHT.read_retry(DHT_TYPE, INPUT_PIN)
 
     if humidity is not None and temperature is not None:
+        # Un-comment the line below to convert the temperature to Fahrenheit.
+        # temperature = temperature * 9/5.0 + 32
         # Prints to terminal
         print 'Temp: {0:0.1f} C  Humidity: {1:0.1f} %'.format(temperature, humidity)
     else:
