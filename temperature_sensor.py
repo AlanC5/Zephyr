5575024ec794a48ca4bf12c2316d634ed66f2348
Alan Chen <alanc1995@gmail.com>
2017-08-26 11:33:13 -0400

    temperature_sensor class

--- temperature_sensor.py
+++ temperature_sensor.py
@@ -11,20 +11,47 @@
 import sys
 import Adafruit_DHT
 
-while True:
-    # Celsius Reading
+class TemperatureHumiditySensor:
+    def __init__(self, INPUT_PIN, DHT_TYPE):
+        self.INPUT_PIN = INPUT_PIN
+        self.DHT_TYPE = DHT_TYPE
+        self.humidity = 0
+        self.temperature = 0
+        self.units = 'celsisu'
+
     # Try to grab a sensor reading.  Use the read_retry method which will retry up
     # to 15 times to get a sensor reading (waiting 2 seconds between each retry).
-    # humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
-    INPUT_PIN = 4
-    DHT_TYPE = Adafruit_DHT.DHT11
+    def read_inputs(self):
+        self.humidity, self.temperature = Adafruit_DHT.read_retry(self.DHT_TYPE, self.INPUT_PIN)
 
-    humidity, temperature = Adafruit_DHT.read_retry(DHT_TYPE, INPUT_PIN)
+    # units to determine temperature units (celsius, fahrenheit)
+    def print_output(self, units='celsius'):
+        if self.humidity is not None and self.temperature is not None:
+            if units == 'fahrenheit':
+                self.temperature = self.temperature * 9/5.0 + 32
+                self.units = units
 
-    if humidity is not None and temperature is not None:
-        # Un-comment the line below to convert the temperature to Fahrenheit.
-        # temperature = temperature * 9/5.0 + 32
-        # Prints to terminal
-        print 'Temp: {0:0.1f} C  Humidity: {1:0.1f} %'.format(temperature, humidity)
-    else:
-        print 'Failed to get reading. Try again!'
+            print 'Temp: {0:0.1f} {1} Humditiy: {2:0.1f} %'.format(self.temperature, units self.humidity)
+        else:
+            print 'Failed to get reading. Try again!'
+
+
+
+
+# while True:
+#     # Celsius Reading
+#     # Try to grab a sensor reading.  Use the read_retry method which will retry up
+#     # to 15 times to get a sensor reading (waiting 2 seconds between each retry).
+#     # humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
+#     INPUT_PIN = 4
+#     DHT_TYPE = Adafruit_DHT.DHT11
+#
+#     humidity, temperature = Adafruit_DHT.read_retry(DHT_TYPE, INPUT_PIN)
+#
+#     if humidity is not None and temperature is not None:
+#         # Un-comment the line below to convert the temperature to Fahrenheit.
+#         # temperature = temperature * 9/5.0 + 32
+#         # Prints to terminal
+#         print 'Temp: {0:0.1f} C  Humidity: {1:0.1f} %'.format(temperature, humidity)
+#     else:
+#         print 'Failed to get reading. Try again!'
